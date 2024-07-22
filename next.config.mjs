56ad9f18a64c02ab693b4dbe27460b69820aa93f
Aliy Akhbar <aliyakhbar@gmail.com>
2024-07-22 22:26:59 +0800

    Push Notification is working

---
+++ next.config.mjs
@@ -0,0 +1,26 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+
+import withSerwistInit from "@serwist/next";
+
+// You may want to use a more robust revision to cache
+// files more efficiently.
+// A viable option is `git rev-parse HEAD`.
+const revision = crypto.randomUUID();
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  cacheOnNavigation: true,
+  swSrc: "src/sw.ts",
+  swDest: "public/sw.js",
+  additionalPrecacheEntries: [{ url: "/~offline", revision }],
+});
+
+export default withSerwist({
+  // Your Next.js config
+  reactStrictMode: true,
+});
---
+++ src/app/~offline/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>Offline Page</div>
+  )
+}
+
+export default page
